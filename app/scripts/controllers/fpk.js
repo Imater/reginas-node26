3c7082a4f74e1b30e6f4021017845dac43d1eb7f
Imater <eugene.leonar@gmail.com>
2013-10-07 14:29:25 +0600

    В тайтл записывается звёздочка при изменении договоров

--- app/scripts/controllers/fpk.js
+++ app/scripts/controllers/fpk.js
@@ -999,6 +999,7 @@ myApp.directive('colorChange', function() {
 
       scope.$watch(attrs.colorChange, function(Val, newVal){
           if(Val != newVal) {
+            document.title = "* ФПК ("+scope.fpk.the_user.fio.split(" ")[0]+") — Регинас";
             if(element.hasClass("changed")) { 
               element.removeClass("changed"); 
               setTimeout(function(){
@@ -1011,6 +1012,7 @@ myApp.directive('colorChange', function() {
             $('body').on("mousemove.my,click,touchdown", function(){
                 clearTimeout(tm_change);
                 tm_change = setTimeout(function(){
+                    document.title = "ФПК ("+scope.fpk.the_user.fio.split(" ")[0]+") — Регинас";
                     $('body').off(".my");
                     $(".changed").removeClass("changed");
                 },200);
