7cbdb19cb709a8f6de84ee22b61d8f4cbb97495e
Imater <eugene.leonar@gmail.com>
2013-10-19 18:43:30 +0600

    Уменьшил кэш

--- server/server.js
+++ server/server.js
@@ -63,7 +63,7 @@ app.configure(function(){
     if(/.(png|jpg|jpeg|woff|gif)/ig.test(req.url)) {
     	res.setHeader("Cache-Control", "public, max-age=17280000");
     } else if( /.(js|css|html|json)/.test(req.url) && !(/localhost/.test(req.headers.host)) ) {
-    	res.setHeader("Cache-Control", "public, max-age=180000");    	
+    	res.setHeader("Cache-Control", "public, max-age=5000");    	
     } else {
     	res.setHeader("Cache-Control", "public, max-age=0");    	
     }
