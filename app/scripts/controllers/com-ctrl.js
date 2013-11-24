f658566ce268c8205e6fd784a503a241e75487aa
Imater <eugene.leonar@gmail.com>
2013-11-24 23:39:48 +0600

    Добавил форму для автомобилей с пробегом

--- app/scripts/controllers/com-ctrl.js
+++ app/scripts/controllers/com-ctrl.js
@@ -2,8 +2,6 @@ myApp.controller('comCtrl', function ($scope, $resource, $rootScope, $location, 
 
 	var client_id = $routeParams.client;
 	var do_id = $routeParams.do;
-	$scope.client = {};
-	$scope.client.dover = "Hi!"+client_id;
 
      $http({url:'/api/v1/com_doc', method: "GET", isArray: true, params: { client_id: client_id, do_id: do_id, brand: $scope.fpk.brand }}).then(function(result){
             $scope.client = result.data;
