71e8b569167816957b79ff59f36dc6b4cbfe5ddc
Imater <eugene.leonar@gmail.com>
2013-11-24 23:11:23 +0600

    Добавил распечатку

--- app/scripts/controllers/com-ctrl.js
+++ app/scripts/controllers/com-ctrl.js
@@ -7,7 +7,7 @@ myApp.controller('comCtrl', function ($scope, $resource, $rootScope, $location, 
 
      $http({url:'/api/v1/com_doc', method: "GET", isArray: true, params: { client_id: client_id, do_id: do_id, brand: $scope.fpk.brand }}).then(function(result){
             $scope.client = result.data;
-            //window.print();
+            window.print();
      });
 
 
